ef4f8b9634aeb1c85709f9fcfbd56b243f5b6eba
wayne.wang <yingxiang907@gmail.com>
2023-02-11 21:20:15 +0800

    try run python action

---
+++ crawl_index.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+def main():
+    index_list = ['000001', '000300', '000905', '000991', '399989', '000993', '000990', '000852', '399967']
+    index_data = []
+    for index in index_list:
+        res = requests.get('https://www.csindex.com.cn/csindex-home/perf/index-perf-oneday?indexCode=' + index)
+        val = res.json().get('data', {}).get('intraDayHeader').get('current')
+        print(index, )
+        index_data.append({
+            'code': index,
+            'value': val
+        })
+    with open('indexData.json', 'w') as f:
+        f.write(json.dumps(index_data))
