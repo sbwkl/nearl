fbfcc33803ee9adbaa5fab4d91228eed3b05f459
wayne.wang <yingxiang907@gmail.com>
2023-02-12 20:21:27 +0800

    show prop

--- crawl_index.py
+++ crawl_index.py
@@ -1,17 +1,21 @@
 import requests
 import json
+from datetime import datetime
 
 def main():
     index_list = ['000001', '000300', '000905', '000991', '399989', '000993', '000990', '000852', '399967']
-    index_data = []
+    item_list = []
     for index in index_list:
         res = requests.get('https://www.csindex.com.cn/csindex-home/perf/index-perf-oneday?indexCode=' + index)
         val = res.json().get('data', {}).get('intraDayHeader').get('current')
-        print(index, val)
-        index_data.append({
+        item_list.append({
             'code': index,
             'value': val
         })
+    index_data = {
+        'gmtModified': int(datetime.now().timestamp()),
+        'itemList': item_list
+    }
     with open('indexData.json', 'w') as f:
         f.write(json.dumps(index_data))
 
