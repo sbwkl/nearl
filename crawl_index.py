7ac738dcc2b1b91af13c313b5115f2c708ec9871
wayne.wang <wayne.wang@beibei.com>
2023-02-13 12:52:37 +0800

    modify

--- crawl_index.py
+++ crawl_index.py
@@ -3,15 +3,18 @@ import json
 from datetime import datetime
 
 def main():
-    index_list = ['000001', '000300', '000905', '000991', '399989', '000993', '000990', '000852', '399967']
+    with open('indexInfo.json', 'r', encoding='UTF-8') as f:
+        indexInfo = json.loads(f.read())
+
+    # index_list = ['000001', '000300', '000905', '000991', '399989', '000993', '000990', '000852', '399967']
     item_list = []
-    for index in index_list:
-        res = requests.get('https://www.csindex.com.cn/csindex-home/perf/index-perf-oneday?indexCode=' + index)
-        val = res.json().get('data', {}).get('intraDayHeader').get('current')
-        item_list.append({
-            'code': index,
-            'value': val
-        })
+    for index in indexInfo:
+    # for index in index_list:
+        try:
+            res = requests.get('https://www.csindex.com.cn/csindex-home/perf/index-perf-oneday?indexCode=' + index.get('code'))
+            item_list.append(res.json().get('data', {}).get('intraDayHeader'))
+        except Exception as e:
+            pass
     index_data = {
         'gmtModified': int(datetime.now().timestamp()),
         'itemList': item_list
